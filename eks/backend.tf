56cab07dddf40a9eb2d815fd3646c1405986a55c
KomalJadhav5602 <166108557+KomalJadhav5602@users.noreply.github.com>
2025-05-21 17:09:33 +0530

    Update backend.tf
    change bucket list

--- eks/backend.tf
+++ eks/backend.tf
@@ -7,7 +7,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "abhya-komal"
+    bucket         = "abhya-komal-116"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
     dynamodb_table = "logfile"
